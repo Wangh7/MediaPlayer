fd4cd12e18b1acd6901f173411db93831ccf8f4c
wangh7 <978102365@qq.com>
2018-11-28 21:28:33 +0800

    增加了 SeekBar 支持SeekBar控制播放位置

--- app/src/main/java/com/example/wangh7/player/MainActivity.java
+++ app/src/main/java/com/example/wangh7/player/MainActivity.java
@@ -2,61 +2,107 @@ package com.example.wangh7.player;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import static android.os.Environment.getExternalStorageDirectory;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
     private MediaPlayer mediaPlayer = new MediaPlayer();
+    private Button play;
+    private Button pause;
+    private Button stop;
+    private SeekBar seekBar;
+    int time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button play = (Button) findViewById(R.id.play);
-        Button pause = (Button) findViewById(R.id.pause);
-        Button stop = (Button) findViewById(R.id.stop);
+        play = (Button) findViewById(R.id.play);
+        pause = (Button) findViewById(R.id.pause);
+        stop = (Button) findViewById(R.id.stop);
 
+        TextView title = (TextView)findViewById(R.id.title_tx);
+        TextView artist = (TextView)findViewById(R.id.artist_tx);
+        TextView album = (TextView)findViewById(R.id.album_tx);
+
+        seekBar = (SeekBar)findViewById(R.id.seekbar);
+        ImageView imageView = (ImageView) findViewById(R.id.image_1);
         play.setOnClickListener(this);
         pause.setOnClickListener(this);
         stop.setOnClickListener(this);
-        if(ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
-        }else {
+        seekBar.setOnSeekBarChangeListener(mOnSeekBarChangeListener);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
             initMediaPlayer();
         }
+        time = mediaPlayer.getDuration();
+        seekBar.setMax(time);
 
     }
 
-    private void initMediaPlayer(){
-        try{
-            File file = new File(Environment.getExternalStorageDirectory(),
+    private SeekBar.OnSeekBarChangeListener mOnSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+            if(mediaPlayer != null) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        }
+    };
+
+    private void initMediaPlayer() {     //初始化播放器
+        try {
+            File file = new File(getExternalStorageDirectory(),
                     "test.mp3");
             mediaPlayer.setDataSource(file.getPath());
             mediaPlayer.prepare();
+            //MediaStore.Audio.Media._ID;
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults){
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case 1:
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     initMediaPlayer();
                 } else {
-                    Toast.makeText(this,"拒绝权限将无法使用程序",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "拒绝权限将无法使用程序", Toast.LENGTH_SHORT).show();
                     finish();
                 }
                 break;
@@ -66,31 +112,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.play:
-                if(!mediaPlayer.isPlaying()) {
+                if (!mediaPlayer.isPlaying()) {
                     mediaPlayer.start();
                 }
                 break;
             case R.id.pause:
-                if(mediaPlayer.isPlaying()) {
+                if (mediaPlayer.isPlaying()) {
                     mediaPlayer.pause();
                 }
                 break;
             case R.id.stop:
-                if(mediaPlayer.isPlaying()) {
+                if (mediaPlayer.isPlaying()) {
                     mediaPlayer.reset();
                     initMediaPlayer();
                 }
                 break;
             default:
-                    break;
+                break;
         }
     }
 
-    protected void onDestory(){
+    protected void onDestory() {
         super.onDestroy();
-        if(mediaPlayer != null) {
+        if (mediaPlayer != null) {
             mediaPlayer.stop();
             mediaPlayer.release();
         }
